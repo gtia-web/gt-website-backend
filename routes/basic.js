71cafcde8aae217aa3c09b2d2e55bdf11e66c2d0
Chidozie Onyeze <chidozieonyeze@gatech.edu>
2021-02-09 01:37:43 -0500

    added template code + template dashboard

--- routes/basic.js
+++ routes/basic.js
@@ -5,9 +5,12 @@ const UserProfile = require('../models/UserProfile');
 const authentication = require("../utility/authentication");
 
 router.get('/', authentication.checkAuthenticated, async (req, res) => {
-    //res.render('index.ejs')
     userData = await UserProfile.findById(req.user._id)
     res.render('index.ejs', {user: userData})
+})
+
+router.get('/dashboard', authentication.checkAuthenticated, async (req, res) => {
+    res.render('dashboard_template.ejs')
 })
 
 
