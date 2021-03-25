fec61b19921d9069167bb99222ce27f8f6366db6
Ishan Baniya <ibaniya98@gmail.com>
2021-03-25 00:20:55 -0400

    Render home page from express with events from facebook

--- routes/basic.js
+++ routes/basic.js
@@ -7,25 +7,25 @@ const authentication = require("../utility/authentication");
 /** 
  * Redirect to Home Page
  */
-router.get('/', authentication.checkAuthenticated, async (req, res) => {
-    res.redirect('/home');
+router.get('/', async (req, res) => {
+    res.render('main.ejs');
 })
 
 /**
  * Get Home Page if Logged in
  */
-router.get('/home', authentication.checkAuthenticated, async (req, res)=> {
+router.get('/home', authentication.checkAuthenticated, async (req, res) => {
     userData = await UserProfile.findById(req.user._id)
-    res.render('home.ejs', {user: userData})
+    res.render('home.ejs', { user: userData })
 })
 
 /**
  * Get Admin Portal Page
  */
-router.get('/admin', authentication.checkAuthenticated, 
+router.get('/admin', authentication.checkAuthenticated,
     authentication.checkAuthenticatedAdmin, async (req, res) => {
-    userData = await UserProfile.findById(req.user._id)
-    res.render('admin_portal.ejs',  {user: userData})
-})
+        userData = await UserProfile.findById(req.user._id)
+        res.render('admin_portal.ejs', { user: userData })
+    })
 
 module.exports = router;
