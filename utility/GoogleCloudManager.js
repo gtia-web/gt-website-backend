5ea560712aca5651c805d507b8d2a27e2dd6358f
Chidozie Onyeze <chidozieonyeze@gatech.edu>
2021-04-06 13:54:13 -0400

    added profile picture changes + reorganized code

--- utility/GoogleCloudManager.js
+++ utility/GoogleCloudManager.js
@@ -123,6 +123,39 @@ async function getImagefromDrive(file_data) {
     
 }
 
+async function getCalenderEvents() {
+    let client = getClient()
+    const calendar = google.calendar({version: 'v3', auth: client});
+    calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+            const events = res.data.items;
+        if (events.length) {
+            console.log('Upcoming 10 events:');
+            events.map((event, i) => {
+                const start = event.start.dateTime || event.start.date;
+                //console.log(`${start} - ${event.summary}`);
+                //console.log(event)
+            });
+
+            return events
+        } else {
+            console.log('No upcoming events found.');
+        }
+    }).then((res) => {
+        console.log(res)
+    });
+
+    
+}
+
+getCalenderEvents()
+
 /**getImagefromDrive({
     Path: "public/uploads/profiles/img",
     Filename: "Test.jpg",
