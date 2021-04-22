4152401be6dff1dcace34d013538045c94b0c5b2
iBaniya <ibaniya98@gmail.com>
2021-04-21 20:43:27 -0400

    Add Contact Us Page (#17)
    * Add basic contact page
    
    * Use gmail to send contact form data
    
    * Change GTIA email as the recipient email

--- routes/basic.js
+++ routes/basic.js
@@ -1,61 +1,106 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserProfile = require('../models/UserProfile');
-const GlobalVariables = require('../models/GlobalVariables');
+const UserProfile = require("../models/UserProfile");
+const GlobalVariables = require("../models/GlobalVariables");
 const authentication = require("../utility/authentication");
 const gcManager = require("../utility/GoogleCloudManager");
-const fs = require('fs')
+const fs = require("fs");
 
-/** 
+/**
  * Redirect to Home Page
  */
-router.get('/', (req, res) => {
-    res.render('sun/index.ejs');
+router.get("/", (req, res) => {
+  res.render("sun/index.ejs");
 });
 
-router.get('/faq', (req, res) => {
-    res.render('sun/faq.ejs');
+router.get("/faq", (req, res) => {
+  res.render("sun/faq.ejs");
 });
 
-router.get('/about', (req, res) => {
-    res.render('sun/about.ejs');
+router.get("/about", (req, res) => {
+  res.render("sun/about.ejs");
 });
 
+router.get("/contact", (req, res) => {
+  res.render("sun/contact.ejs", {
+    success: false,
+    error: false,
+  });
+});
+
+router.post("/contact", async (req, res) => {
+  try {
+    const { name, email, subject, message } = req.body;
+    const mailOptions = {
+      from: email,
+      to: "gtinternationalambassadors@gmail.com",
+      subject: subject,
+      text: `${name} says\n\n${message}`,
+    };
+
+    const transporter = await gcManager.getEmailTransporter();
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.render("sun/contact.ejs", {
+          success: false,
+          error: true,
+        });
+      }
+      res.render("sun/contact.ejs", {
+        success: true,
+        error: false,
+      });
+    });
+  } catch (err) {
+    res.render("sun/contact.ejs", {
+      success: false,
+      error: true,
+    });
+  }
+});
 
 /**
  * Get Home Page if Logged in
  */
-router.get('/home', authentication.checkAuthenticated, async (req, res)=> {
-    let userData = await UserProfile.findById(req.user._id)
-    let profileFilePath = 'public/' + userData.ProfilePicture.Path + '/' + userData.ProfilePicture.Filename
-    if (!fs.existsSync(profileFilePath)) {
-        await gcManager.getImagefromDrive({
-            Path: 'public/' + userData.ProfilePicture.Path,
-            Filename: userData.ProfilePicture.Filename,
-            FileID: userData.ProfilePicture.GCImageID
-        })
-    }
+router.get("/home", authentication.checkAuthenticated, async (req, res) => {
+  let userData = await UserProfile.findById(req.user._id);
+  let profileFilePath =
+    "public/" +
+    userData.ProfilePicture.Path +
+    "/" +
+    userData.ProfilePicture.Filename;
+  if (!fs.existsSync(profileFilePath)) {
+    await gcManager.getImagefromDrive({
+      Path: "public/" + userData.ProfilePicture.Path,
+      Filename: userData.ProfilePicture.Filename,
+      FileID: userData.ProfilePicture.GCImageID,
+    });
+  }
 
-    let globalVariables = await GlobalVariables.findById(process.env.GLOBAL_VARIABLES_ID)
-    res.render('moon/home.ejs', {
-        user: userData,
-        globalVariables: globalVariables
-    })
-})
+  let globalVariables = await GlobalVariables.findById(
+    process.env.GLOBAL_VARIABLES_ID
+  );
+  res.render("moon/home.ejs", {
+    user: userData,
+    globalVariables: globalVariables,
+  });
+});
 
 /**
  * Get Admin Portal Page
  */
-router.get('/admin', authentication.checkAuthenticated,
-    authentication.checkAuthenticatedAdmin, async (req, res) => {
-        let userData = await UserProfile.findById(req.user._id)
-        res.render('moon/admin_portal.ejs',  {user: userData})
-})
+router.get(
+  "/admin",
+  authentication.checkAuthenticated,
+  authentication.checkAuthenticatedAdmin,
+  async (req, res) => {
+    let userData = await UserProfile.findById(req.user._id);
+    res.render("moon/admin_portal.ejs", { user: userData });
+  }
+);
 
-router.get('/image', (req, res) => {
-    res.render('moon/imageUpload.ejs');
+router.get("/image", (req, res) => {
+  res.render("moon/imageUpload.ejs");
 });
-
-
 
 module.exports = router;
